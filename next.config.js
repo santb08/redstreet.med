1feb7010daaa04e84b2bd13c63dd0ffc6ae29e61
Santiago <scb9908@gmail.com>
2022-07-06 18:55:45 -0500

    fix: cors allowing

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   headers: async () => [{
     // matching all API routes
-    source: "/api/:path*",
+    source: "/graphql/*",
     headers: [
       { key: "Access-Control-Allow-Credentials", value: "true" },
       { key: "Access-Control-Allow-Origin", value: "*" },

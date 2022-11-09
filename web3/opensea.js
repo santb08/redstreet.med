e7f89deeffa820e3c465d916d0d2146e0e95278e
Cristian Serna <Cristianxz999@gmail.com>
2022-11-09 15:34:38 -0500

    fix: coments done

--- web3/opensea.js
+++ web3/opensea.js
@@ -1,17 +1,9 @@
 import axios from  'axios'
 const BASE_URL =
   "https://testnets-api.opensea.io/api/v1/assets?owner=0x6e4AE7C8d14F93382A92543aa2113c6F18C9F9aa";
-const config = {
-  method: "get",
-  url: BASE_URL,
-  headers: {
-    Cookie:
-      "__cf_bm=2J0AQb4NHejLLytHsjZ7HOQWA2Ht8A1ND_YOPomPpmA-1668022809-0-AZAZYtt7gbzQBhnOb8lj0/qChrfxEgfKILQK8Yd1Bv+QIYkxWdweBPXt7R6bm1uYf+FQKt0xX0zpiBeKXRgugXo=",
-  },
-};
 
-export const getImages = () => {
-  const data = axios(config)
+export const getImages = async () => {
+  axios.get(BASE_URL)
     .then(function (response) {
       return response.data
     })

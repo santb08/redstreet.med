c36b4368acbd5c0e8ed690b614ebb5a4bbce0f9a
Cristian Serna <Cristianxz999@gmail.com>
2022-11-09 15:40:05 -0500

    fix: coments redone

--- web3/opensea.js
+++ web3/opensea.js
@@ -2,13 +2,12 @@ import axios from  'axios'
 const BASE_URL =
   "https://testnets-api.opensea.io/api/v1/assets?owner=0x6e4AE7C8d14F93382A92543aa2113c6F18C9F9aa";
 
-export const getImages = async () => {
-  axios.get(BASE_URL)
-    .then(function (response) {
-      return response.data
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  return data
-};
+  export const getImages = async () => {
+    try {
+       const data = await axios.get(BASE_URL);
+       return data;
+    } catch (error) {
+       console.error(error);
+       return [];
+    }
+ };
